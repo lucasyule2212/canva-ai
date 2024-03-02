869b8f52e499f06bec27b98481bfb414208fbb17
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-01 23:45:12 -0300

    feat(auth-layout): adding base auth layout component

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className='auth'>{children}</main>
+  )
+}
+
+export default AuthLayout
