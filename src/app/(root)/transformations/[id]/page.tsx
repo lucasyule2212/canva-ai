ee2490b513fbfed2bfdd73888a8ec06b55e79ced
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-01 23:55:26 -0300

    feat(root-folders): adding base root folders/pages structure

---
+++ src/app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,8 @@
+
+const TransformationsPage = () => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
---
+++ src/app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,8 @@
+
+const UpdateTransformationPage = () => {
+  return (
+    <div>UpdateTransformationPage</div>
+  )
+}
+
+export default UpdateTransformationPage
