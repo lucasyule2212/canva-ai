fbe1140b52f60ca57a5663f04837e3669dfa1163
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-04 12:56:00 -0300

    refactor(styles): adding custom colors to tailwind.config + tailwind utility styles to globals.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,19 @@ const config = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        purple: {
+          100: "#F4F7FE",
+          200: "#BCB6FF",
+          400: "#868CFF",
+          500: "#7857FF",
+          600: "#4318FF",
+        },
+        dark: {
+          400: "#7986AC",
+          500: "#606C80",
+          600: "#2B3674",
+          700: "#384262",
+        },
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
@@ -52,6 +65,12 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+      },
+      fontFamily: {
+        IBMPlex: ["var(--font-ibm-plex)"],
+      },
+      backgroundImage: {
+        "purple-gradient": "url('/assets/images/gradient-bg.svg')",
       },
       borderRadius: {
         lg: "var(--radius)",
