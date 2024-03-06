8fca4999fd611043609e1165498f37b6e4999c8c
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-05 23:23:10 -0300

    feat(mobile-nav): adding base mobile-nav component

---
+++ src/components/shared/sidebar/mobile-nav.tsx
@@ -0,0 +1,60 @@
+'use client'
+import { Button } from "@/components/ui/button"
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger
+} from "@/components/ui/sheet"
+import { navLinks } from "@/lib/consts"
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from "next/navigation"
+
+const MobileNav = () => {
+  const pathname = usePathname()
+  return (
+    <header className='flex-between fixed h-16 w-full border-b-4 border-purple-100 bg-white p-5 lg:hidden'>
+      <Link href='/' className='flex items-center gap-2 md:py-2'>
+        <Image src='/assets/images/logo-text.svg' alt='logo' width={180} height={28}/>
+      </Link>
+      <nav className='flex gap-2'>
+        <SignedIn>
+          <UserButton afterSignOutUrl='/' />
+          <Sheet>
+            <SheetTrigger>
+              <Image src='/assets/icons/menu.svg' alt='menu' width={32} height={32} className="cursor-pointer"/>
+            </SheetTrigger>
+            <SheetContent className="focus:ring-0 focus-visible:ring-transparent focus:ring-offset-0 focus-visible:ring-offset-0 focus-visible:outline-none focus-visible:border-none sm:w-64">
+              <>
+                <Image src='/assets/images/logo-text.svg' alt='logo' width={152} height={23} />
+                <ul className="mt-8 flex w-full flex-col items-start gap-5">
+                  {navLinks.map((link) => {
+                    const isActive = pathname === link.route
+                    return (
+                      <li key={link.route} data-isactive={isActive}
+                      className="data-[isactive=true]:gradient-text p-18 flex whitespace-nowrap text-dark-700"
+                      >
+                        <Link key={link.label} href={link.route} className="p-16-semibold flex size-full gap-4 p-4 cursor-pointer ">
+                          <Image alt={link.label} width={24} height={24} src={link.icon}/>
+                          <span>{link.label}</span>
+                        </Link>
+                      </li>
+                    )
+                  })}
+                </ul>
+              </>
+            </SheetContent>
+          </Sheet>
+        </SignedIn>
+        <SignedOut>
+            <Button asChild className="py-4 px-6 flex-center gap-3 rounded-full p-16-semibold focus-visible:ring-offset-0 focus-visible:ring-transparent bg-purple-gradient bg-cover">
+              <Link href='/sign-in'/>
+            </Button>
+        </SignedOut>
+      </nav>
+    </header>
+  )
+}
+
+export default MobileNav
