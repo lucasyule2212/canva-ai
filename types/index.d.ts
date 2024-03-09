9b52388d889f69594bd55b6bf11e5f5619956f6b
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-08 23:37:52 -0300

    feat(types-folder): adding base global types archive into types folder

---
+++ types/index.d.ts
@@ -0,0 +1,16 @@
+// ====== USER PARAMS
+declare type CreateUserParams = {
+  clerkId: string;
+  email: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  photo: string;
+};
+
+declare type UpdateUserParams = {
+  firstName: string;
+  lastName: string;
+  username: string;
+  photo: string;
+};
