29c26b8333ff5fb3aa5ce4d348bee2936b6136c8
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-10 23:27:23 -0300

    feat(header-component): adding base shared header componen

---
+++ src/components/shared/header/header.tsx
@@ -0,0 +1,18 @@
+
+interface HeaderProps {
+  title: string
+  subtitle?: string
+}
+
+const Header = ({title,subtitle}:HeaderProps) => {
+  return (
+    <>
+      <h2 className="h2-bold text-dark-600">
+        {title}
+      </h2>
+      {subtitle && <p className="p-16-regular mt-4">{subtitle}</p>}
+    </>
+  )
+}
+
+export default Header
