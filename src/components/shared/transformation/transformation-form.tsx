6b20c265a7fcc09fd4fa3e26cc32f5a075c6ffbb
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-11 23:54:18 -0300

    refactor(transformation-form): updating transformation-form base schema and props + initial values

--- src/components/shared/transformation/transformation-form.tsx
+++ src/components/shared/transformation/transformation-form.tsx
@@ -1,30 +1,33 @@
 "use client"
-import { Button } from "@/components/ui/button"
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+  Form
 } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import { defaultValues } from "@/lib/consts"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
 const formSchema = z.object({
-  username: z.string().min(2).max(50),
+  title: z.string(),
+  aspectRatio: z.string().optional(),
+  color: z.string().optional(),
+  prompt: z.string().optional(),
+  publicId: z.string()
 })
 
 
-const TransformationForm = () => {
+const TransformationForm = ({action, data=null}:TransformationFormProps) => {
+  const initialValues = data && action === 'Update' ? {
+    title: data?.title,
+    aspectRatio: data?.aspectRatio,
+    color: data?.color,
+    prompt: data?.prompt,
+    publicId: data?.publicId
+  } : defaultValues;
+  
     const form = useForm<z.infer<typeof formSchema>>({
       resolver: zodResolver(formSchema),
-      defaultValues: {
-        username: "",
-      },
+      defaultValues: initialValues
     })
    
     function onSubmit(values: z.infer<typeof formSchema>) {
@@ -34,23 +37,7 @@ const TransformationForm = () => {
     return (
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
+          
         </form>
       </Form>
     )
