29395fbfc6f615d0c595003a457062dbe27f3b7e
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-11 23:55:04 -0300

    feat(transformations-add): adding user handler to transformations-add page

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,18 +1,32 @@
 import Header from '@/components/shared/header/header';
 import TransformationForm from '@/components/shared/transformation/transformation-form';
+import { getUserById } from '@/lib/actions/user.actions';
 import { transformationTypes } from '@/lib/consts';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
 
-const AddTransformationTypePage = ({ params: { type } }: {
+const AddTransformationTypePage = async ({ params: { type } }: {
   params: {
     type: string
   }
 }) => {
+  const { userId } = auth()
+  
   const transformation = transformationTypes[type];
+
+  if (!userId) redirect('/sign-in');
+
+  const user = await getUserById(userId)
   return (
     <div>
       <Header title={transformation.title} subtitle={transformation.subTitle} />
-      <TransformationForm/>
+      <TransformationForm
+        action='Add'
+        userId={user}
+        type={transformation.type as TransformationTypeKey} 
+        creditBalance={user.creditBalance}
+      />
     </div>
   )
 }
