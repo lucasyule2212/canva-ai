c3c1878b07740c7d333ca5c87172955161d9ad7f
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-15 16:24:13 -0300

    feat(media-uploader): finalizing media-uploader component logic + utilities functions

--- src/components/shared/media-uploader/media-uploader.tsx
+++ src/components/shared/media-uploader/media-uploader.tsx
@@ -1,9 +1,13 @@
-import { useToast } from '@/components/ui/use-toast'
-import { CldUploadWidget } from 'next-cloudinary'
+'use client'
+import { useToast } from '@/components/ui/use-toast';
+import { dataUrl, getImageSize } from '@/lib/utils';
+import { CldImage, CldUploadWidget } from 'next-cloudinary';
+import { PlaceholderValue } from 'next/dist/shared/lib/get-img-props';
+import Image from 'next/image';
 
 interface MediaUploaderProps {
   onValueChange: (value: string) => void;
-  setImage: (image: any) => void;
+  setImage: React.Dispatch<any>;
   image: any;
   publicId: string;
   type: string;
@@ -13,10 +17,28 @@ const MediaUploader = ({image,setImage,publicId,type,onValueChange}:MediaUploade
   const { toast } = useToast()
 
   const onUploadSuccessHandler = (result: any) => {
-
+    setImage((prevState: any) => ({
+      ...prevState,
+      publicId: result?.info?.public_id,
+      width: result?.info?.width,
+      height: result?.info?.height,
+      secureUrl: result?.info?.secure_url
+    }))
+    onValueChange(result?.info?.public_id)
+    toast({
+      title: 'Image uploaded successfully!',
+      description: 'You have used 1 credit',
+      duration: 5000,
+      variant:'success'
+    })
   }
   const onUploadErrorHandler = (error: any) => {
-
+    toast({
+      title: 'Error',
+      description: 'There was an error uploading the image',
+      duration: 5000,
+      variant:'destructive'
+    })
   }
 
   return (
@@ -28,7 +50,34 @@ const MediaUploader = ({image,setImage,publicId,type,onValueChange}:MediaUploade
       }}
       onSuccess={onUploadSuccessHandler}
       onError={onUploadErrorHandler}
-    />
+    >
+      {({ open }) => (
+        <div className='flex flex-full flex-col gap-4'>
+          <h3 className='h3-bold text-dark-600'>
+            Original
+          </h3>
+          {publicId ?
+            <div className='cursor-pointer overflow-hidden rounded-[10px]'>
+              <CldImage
+                className='h-fit min-h-72 w-full rounded-[10px] border border-dashed bg-purple-100/20 object-cover p-2'
+                width={getImageSize(type, image, "width")}
+                height={getImageSize(type, image, "width")}
+                src={publicId} alt="image" sizes={"(max-width: 767px) 100vw, 50vw"}
+                placeholder={dataUrl as PlaceholderValue}
+              />
+            </div>
+            :
+            <div className='flex-center flex h-72 cursor-pointer flex-col gap-5 rounded-[16px] border border-dashed bg-purple-100/20 shadow-inner' onClick={()=>open()}>
+              <div className='rounded-[16px] bg-white  p-5 shadow-sm shadow-purple-200/50'>
+                <Image src='/assets/icons/add.svg' alt='Add Image' width={24} height={24} />
+              </div>
+              <p className='p-14-medium'>Click here to upload image</p>
+            </div>
+          }
+        </div>
+      )}
+    </CldUploadWidget>
+
   )
 }
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { aspectRatioOptions } from "./consts";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -56,3 +57,43 @@ export const deepMergeObjects = (obj1: any, obj2: any) => {
 
   return output;
 };
+
+// IMAGE SIZE
+export type AspectRatioKey = keyof typeof aspectRatioOptions;
+export const getImageSize = (
+  type: string,
+  image: any,
+  dimension: "width" | "height"
+): number => {
+  if (type === "fill") {
+    return (
+      aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[dimension] ||
+      1000
+    );
+  }
+  return image?.[dimension] || 1000;
+};
+
+// PLACEHOLDER LOADER
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#7986AC" offset="20%" />
+      <stop stop-color="#68769e" offset="50%" />
+      <stop stop-color="#7986AC" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#7986AC" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+const toBase64 = (str: string) =>
+  typeof window === "undefined"
+    ? Buffer.from(str).toString("base64")
+    : window.btoa(str);
+    
+export const dataUrl = `data:image/svg+xml;base64,${toBase64(
+  shimmer(1000, 1000)
+)}`;
