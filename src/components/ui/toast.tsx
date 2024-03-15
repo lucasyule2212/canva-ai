68701748cdb91023526fdb89b55226af38bd58dc
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-15 16:23:27 -0300

    refactor(shadcn-toast): adding success variant to toast component

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -32,6 +32,7 @@ const toastVariants = cva(
         default: "border bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
+        success: "bg-green-100 text-green-900 border-green-200",
       },
     },
     defaultVariants: {
