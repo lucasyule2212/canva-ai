847155703523b487853484c750df3f46584d8a54
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-18 14:24:20 -0300

    feat(image.actions): adding image server actions handlers + params types

--- types/index.d.ts
+++ types/index.d.ts
@@ -17,6 +17,43 @@ declare type UpdateUserParams = {
 
 // ====== IMAGE PARAMS
 
+declare type AddImageParams = {
+  image: {
+    title: string;
+    publicId: string;
+    transformationType: string;
+    width: number;
+    height: number;
+    config: any;
+    secureURL: string;
+    transformationURL: string;
+    aspectRatio: string | undefined;
+    prompt: string | undefined;
+    color: string | undefined;
+  };
+  userId: string;
+  path: string;
+};
+
+declare type UpdateImageParams = {
+  image: {
+    _id: string;
+    title: string;
+    publicId: string;
+    transformationType: string;
+    width: number;
+    height: number;
+    config: any;
+    secureURL: string;
+    transformationURL: string;
+    aspectRatio: string | undefined;
+    prompt: string | undefined;
+    color: string | undefined;
+  };
+  userId: string;
+  path: string;
+};
+
 declare type Transformations = {
   restore?: boolean;
   fillBackground?: boolean;
