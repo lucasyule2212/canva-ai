2856db581dc55b46acddad6b4fbc466cd4eb095f
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-23 17:54:11 -0300

    refactor(transformed-image): implementing downloadHandler function

--- src/components/shared/transformed-image/transformed-image.tsx
+++ src/components/shared/transformed-image/transformed-image.tsx
@@ -1,18 +1,33 @@
-import { dataUrl, debounce, getImageSize } from '@/lib/utils'
-import { CldImage } from 'next-cloudinary'
+'use client'
+import { dataUrl, debounce, download, getImageSize } from '@/lib/utils'
+import { CldImage, getCldImageUrl } from 'next-cloudinary'
 import { PlaceholderValue } from 'next/dist/shared/lib/get-img-props'
 import Image from 'next/image'
 
 const TransformedImage = ({
   image,
   type,
-  // title,
+  title,
   transformationConfig,
   isTransforming,
   setIsTransforming,
   hasDownload = false,
 }: TransformedImageProps) => {
-  const downloadHandler = () => {}
+  const downloadHandler = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    e.preventDefault()
+
+    download(
+      getCldImageUrl({
+        width: image?.width,
+        height: image?.height,
+        src: image?.publicId,
+        ...transformationConfig,
+      }),
+      title,
+    )
+  }
   return (
     <div className="flex flex-col gap-4">
       <div className="flex-between">
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -130,3 +130,24 @@ export function removeKeysFromQuery({
 
   return `${window.location.pathname}?${qs.stringify(currentUrl)}`
 }
+
+// DOWNLOAD IMAGE
+export const download = (url: string, filename: string) => {
+  if (!url) {
+    throw new Error('Resource URL not provided! You need to provide one')
+  }
+
+  fetch(url)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const blobURL = URL.createObjectURL(blob)
+      const a = document.createElement('a')
+      a.href = blobURL
+
+      if (filename && filename.length)
+        a.download = `${filename.replace(' ', '_')}.png`
+      document.body.appendChild(a)
+      a.click()
+    })
+    .catch((error) => console.log({ error }))
+}
