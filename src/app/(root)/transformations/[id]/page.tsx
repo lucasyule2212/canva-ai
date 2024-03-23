6c0b68f6d1269609fb270b695d2aa137bccb38c0
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-23 17:57:59 -0300

    feat(transformation-page): adding transformation dynamic page

--- src/app/(root)/transformations/[id]/page.tsx
+++ src/app/(root)/transformations/[id]/page.tsx
@@ -1,5 +1,106 @@
-const TransformationsPage = () => {
-  return <div>TransformationsPage</div>
+import { auth } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import DeleteConfirmation from '@/components/shared/delete-confirmation/delete-confirmation'
+import Header from '@/components/shared/header/header'
+import TransformedImage from '@/components/shared/transformed-image/transformed-image'
+import { Button } from '@/components/ui/button'
+import { getImageById } from '@/lib/actions/image.actions'
+import { getImageSize } from '@/lib/utils'
+
+const TransformationPage = async ({ params: { id } }: SearchParamProps) => {
+  const { userId } = auth()
+
+  const image = await getImageById(id)
+
+  return (
+    <>
+      <Header title={image.title} />
+
+      <section className="mt-5 flex flex-wrap gap-4">
+        <div className="p-14-medium md:p-16-medium flex gap-2">
+          <p className="text-dark-600">Transformation:</p>
+          <p className=" capitalize text-purple-400">
+            {image.transformationType}
+          </p>
+        </div>
+
+        {image.prompt && (
+          <>
+            <p className="hidden text-dark-400/50 md:block">&#x25CF;</p>
+            <div className="p-14-medium md:p-16-medium flex gap-2 ">
+              <p className="text-dark-600">Prompt:</p>
+              <p className=" capitalize text-purple-400">{image.prompt}</p>
+            </div>
+          </>
+        )}
+
+        {image.color && (
+          <>
+            <p className="hidden text-dark-400/50 md:block">&#x25CF;</p>
+            <div className="p-14-medium md:p-16-medium flex gap-2">
+              <p className="text-dark-600">Color:</p>
+              <p className=" capitalize text-purple-400">{image.color}</p>
+            </div>
+          </>
+        )}
+
+        {image.aspectRatio && (
+          <>
+            <p className="hidden text-dark-400/50 md:block">&#x25CF;</p>
+            <div className="p-14-medium md:p-16-medium flex gap-2">
+              <p className="text-dark-600">Aspect Ratio:</p>
+              <p className=" capitalize text-purple-400">{image.aspectRatio}</p>
+            </div>
+          </>
+        )}
+      </section>
+
+      <section className="mt-10 border-t border-dark-400/15">
+        <div className="grid h-fit min-h-[200px] grid-cols-1 gap-5 py-8 md:grid-cols-2">
+          {/* MEDIA UPLOADER */}
+          <div className="flex flex-col gap-4">
+            <h3 className="h3-bold text-dark-600">Original</h3>
+
+            <Image
+              width={getImageSize(image.transformationType, image, 'width')}
+              height={getImageSize(image.transformationType, image, 'height')}
+              src={image.secureURL}
+              alt="image"
+              className="transformation-original_image"
+            />
+          </div>
+
+          {/* TRANSFORMED IMAGE */}
+          <TransformedImage
+            image={image}
+            type={image.transformationType}
+            title={image.title}
+            isTransforming={false}
+            transformationConfig={image.config}
+            hasDownload={true}
+          />
+        </div>
+
+        {userId === image.author.clerkId && (
+          <div className="mt-4 space-y-4">
+            <Button
+              asChild
+              type="button"
+              className="p-16-semibold h-[50px] w-full rounded-full bg-purple-gradient bg-cover px-6 py-4 capitalize md:h-[54px]"
+            >
+              <Link href={`/transformations/${image._id}/update`}>
+                Update Image
+              </Link>
+            </Button>
+
+            <DeleteConfirmation imageId={image._id} />
+          </div>
+        )}
+      </section>
+    </>
+  )
 }
 
-export default TransformationsPage
+export default TransformationPage
