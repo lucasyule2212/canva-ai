90ba5d3591e9de20425ab7e019c1cac306adb10e
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-28 16:34:36 -0300

    fix(middleware): fixing public routes at clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ['/api/webhooks/clerk'],
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe'],
   // Routes that can always be accessed, and have
   // no authentication information
   ignoredRoutes: ['/no-auth-in-this-route'],
